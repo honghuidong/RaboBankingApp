3819dc398da99a365ef1cfa40e1f58be96347261
Hong Hui <hong.dong@rabobank.com>
2023-03-15 21:01:19 +0100

    add initial project

---
+++ RaboBankingAppServer/DataContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RaboBankingAppServer.Entities;
+using Account = RaboBankingAppServer.Entities.Account;
+using Transaction = RaboBankingAppServer.Entities.Transaction;
+
+namespace RaboBankingAppServer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            //Database.EnsureCreated(); //before you run our application you make sure that our tables are there
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Transaction>()
+            //    .ToTable("Transactions")
+            //    .HasKey(c => c.TransactionId);
+
+            //modelBuilder.Entity<UserAccount>()
+            //    .ToTable("UserAccounts")
+            //    .HasKey(c => c.UserAccountId);
+
+            //modelBuilder.Entity<Account>()
+            //    .ToTable("Accounts")
+            //    .HasKey(c => c.AccountId);
+
+            //modelBuilder
+            //    .Entity<Transaction>()
+            //    .HasOne<Account>(a => a.FromAccount)
+            //    //.WithMany(m => m.FromTransactions)
+            //    //.HasForeignKey(a => a.FromAccountId)
+            //    //.OnDelete(DeleteBehavior.NoAction)
+            //    .HasPrincipalKey(p => p.Id);
+
+            //modelBuilder
+            //    .Entity<Transaction>()
+            //    .HasOne<Account>(a => a.ToAccount)
+            //    //.WithMany(m => m.ToTransactions)
+            //    .HasForeignKey(a => a.ToAccountId)
+            //    .OnDelete(DeleteBehavior.NoAction)
+            //    .HasPrincipalKey(p => p.Id);
+        }
+    }
+}
---
+++ RaboBankingAppServer/Migrations/20230313144623_Initial Migration.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaboBankingAppServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Keywords = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Iban = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Balance = table.Column<double>(type: "float", nullable: true),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    BalanceAfterBooking = table.Column<double>(type: "float", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CarbonFootPrint = table.Column<double>(type: "float", nullable: false),
+                    IncomingTransaction = table.Column<bool>(type: "bit", nullable: false),
+                    FromAccountId = table.Column<int>(type: "int", nullable: false),
+                    ToAccountId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Accounts_FromAccountId",
+                        column: x => x.FromAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Accounts_ToAccountId",
+                        column: x => x.ToAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_CategoryId",
+                table: "Accounts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_FromAccountId",
+                table: "Transactions",
+                column: "FromAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ToAccountId",
+                table: "Transactions",
+                column: "ToAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
